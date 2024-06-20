2b915e935fb88abd54fa453484986e537355ee1f
Everton Braga <evertonr.braga@gmail.com>
2024-06-20 19:21:09 -0300

    type template fixes, default color updates

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -2,20 +2,13 @@ export const defaultTheme = {
   white: '#FFF',
 
   gray100: '#E1E1E6',
-  gray300: '#C4C4CC',
-  gray400: '#8D8D99',
-  gray500: '#7C7C8A',
-  gray600: '#323238',
-  gray700: '#29292E',
-  gray800: '#202024',
-  gray900: '#121214',
+  gray200: '#C4C4CC',
+  gray300: '#8D8D99',
+  gray400: '#7C7C8A',
+  gray500: '#323238',
+  gray600: '#29292E',
+  gray700: '#202024',
+  gray800: '#121214',
 
-  green300: '#00B37E',
-  green500: '#00875F',
-  green700: '#015F43',
-
-  red500: '#AB222E',
-  red700: '#7A1921',
-
-  yellow500: '#FBA94C'
+  black: '#000'
 } as const
